c5f402e6f2a2463d9fbf1fd20efec984ee9fa2b0
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2022-06-22 12:23:58 +0630

    update

--- utils/http.js
+++ utils/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const host = "http://150.95.82.125:4040";
-export const host = "http://localhost:4040";
+export const host = "http://150.95.82.125:4040";
+// export const host = "http://localhost:4040";
 
 const getAuthHeader = () => ({
   Authorization: `Bearer ${localStorage.getItem("token")}`,
